c871f40feb9d46a42777719dc818fe802a52dcc9
David Robinson <dgrtwo@princeton.edu>
2015-04-25 16:48:59 -0400

    Fixed some issues in R file

--- R/stackme.R
+++ R/stackme.R
@@ -1,10 +1,12 @@
-.onLoad <- function(libname, pkgname) {
-  options(error = function(e) options(last_error = geterrmessage()))
-}
-
 #' Search Stack Overflow for your most recent error message.
 #'
 #' @export
 stackme <- function() {
-  browseUrl(paste0("http://stackoverflow.com/?q=", URLencode(getOption("lasterror"))
+  last_error <- geterrmessage()
+  if (is.null(last_error)) {
+    stop("No error message available")
+  }
+
+  query <- URLencode(paste("[r]", last_error))
+  browseURL(paste0("http://stackoverflow.com/search?q=", query))
 }
